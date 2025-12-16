449059cb6b5023dfbc5d0908b78c0d6501f2a3be
Your Name <you@example.com>
2025-12-16 19:04:28 -0300

    seguimos ig

--- Back/index.js
+++ Back/index.js
@@ -1,6 +1,6 @@
 import {subscribePOSTEvent, subscribeGETEvent, startServer} from "soquetic"
-import fs from "fs"; //Anotación: fs es el módulo del sistema de archivos que permite leer y escribir archivos
-startServer()
+import fs from "fs"; //Anotación: node fs nos permite poder usar java para, ademas de hacer paginas web, programar el back end,
+startServer()        //osea correr el codigo desde fuera del navegador.
 
 subscribeGETEvent("obras", obras)
 subscribeGETEvent("colección", coleccion)
@@ -17,27 +17,21 @@ function coleccion() {                  //Anontacion: JSON.parse interpreta text
     return contenidoColeccion
 }
 //Anotación: Esta función modifica el archivo colección.json agregando o quitando el ID de la obra según el valor de enColección
-function modificarColeccion(data) {
-
-    let coleccionActual = JSON.parse(fs.readFileSync("../data/coleccion.json","utf-8"));
-    
+function modificarColeccion(data) {//Anotación: data es el parametro que recibe la función modificarColección
+                        //Anotación: data se convierte en el parametro de modificarColección, que es un objeto con dos propiedades: id y enColección
+    let coleccionActual = JSON.parse(fs.readFileSync("../data/coleccion.json","utf-8"));//Anotación: coleccionActual es un array con los IDs de las obras que están en la colección
     if (data.enColección === true) {
         
-        if (!coleccionActual.includes(data.id)) {
+        if (!coleccionActual.includes(data.id)) { //Anotación: el segundo if evita que haya duplicados en el array
             coleccionActual.push(data.id); //Anotación: push agrega un elemento al final del array que se encuentra en coección.json
             
             fs.writeFileSync("../data/coleccion.json", JSON.stringify(coleccionActual,null, 2));
-            
-            return true;
         }
-        
         return true;
     } else {
-        
+                                          //Anotación: filter crea un nuevo array con los elementos que cumplen la condición, los que no seran eliminados
         coleccionActual = coleccionActual.filter(id => id !== data.id); //Anotación: !== quiere decir si no es igual a...
-        
         fs.writeFileSync("../data/coleccion.json", JSON.stringify(coleccionActual,null, 2));
-        
         return true;
     }
 }
@@ -47,5 +41,5 @@ function obrasColeccion() {
     let todasLasObras = JSON.parse(fs.readFileSync("../data/obras.json","utf-8"));
     let idColeccion = JSON.parse(fs.readFileSync("../data/coleccion.json","utf-8"));
     let obrasEnColeccion = todasLasObras.filter(obra => idColeccion.includes(obra.id));
-    return obrasEnColeccion;
+    return obrasEnColeccion;             //Anotción: Ademas, filter filtra todos los elementos, no uno en especifico
 }
